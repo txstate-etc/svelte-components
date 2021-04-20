e0407f5642eff7e2678b294e8289c89800f90a5e
Nick Wing <nwing@txstate.edu>
2021-04-20 02:34:26 -0500

    in use:offset action, separate out the logic for watching for offset changes so it can be re-used in other actions

--- src/actions/offset.ts
+++ src/actions/offset.ts
@@ -1,60 +1,34 @@
-import equal from 'fast-deep-equal'
-import { bodyOffset, debounced, SettableSubject } from '../lib'
+import { debounced, WritableSubject, ElementOffsets, watchForPositionChange } from '../lib'
 
-export type ElementOffsets = Partial<ReturnType<typeof bodyOffset>>
-
-interface OffsetConfig {
+interface OffsetConfig<T extends ElementOffsets = ElementOffsets> {
   debounce?: boolean|number
-  store?: SettableSubject<ElementOffsets>
+  store?: WritableSubject<T>
 }
 
 export function offset (el: HTMLElement, config?: OffsetConfig) {
   if (config?.debounce === true) config.debounce = 100
   let lastOffset: ElementOffsets = {}
-  function lookforoffsetchange () {
-    const current = bodyOffset(el)
-    if (!equal(lastOffset, current)) {
-      lastOffset = current
-      if (config?.store) config.store.set(current)
-      el.dispatchEvent(new CustomEvent('offset', { detail: current }))
-    }
+  function lookforoffsetchange (current: ElementOffsets) {
+    lastOffset = current
+    if (config?.store) config.store.update(v => ({ ...v, ...current }))
+    el.dispatchEvent(new CustomEvent('offset', { detail: current }))
   }
   let resolvedchange = config?.debounce ? debounced(lookforoffsetchange, config.debounce) : lookforoffsetchange
 
-  window.addEventListener('resize', resolvedchange, { passive: true })
-  let observer = new MutationObserver(resolvedchange)
-  observer.observe(document.body, {
-    subtree: true,
-    childList: true,
-    attributes: true,
-    characterData: true
-  })
-  lookforoffsetchange()
+  const { destroy, update } = watchForPositionChange(el, resolvedchange)
 
   return {
     update (newConfig?: OffsetConfig) {
       if (newConfig?.debounce === true) newConfig.debounce = 100
       if (newConfig?.store !== config?.store) {
-        newConfig?.store?.set(lastOffset)
+        newConfig?.store?.update(v => ({ ...v, ...lastOffset }))
       }
       if (newConfig?.debounce !== config?.debounce) {
-        window.removeEventListener('resize', resolvedchange)
         resolvedchange = newConfig?.debounce ? debounced(lookforoffsetchange, newConfig.debounce as any) : lookforoffsetchange
-        window.addEventListener('resize', resolvedchange, { passive: true })
-        observer.disconnect()
-        observer = new MutationObserver(resolvedchange)
-        observer.observe(document.body, {
-          subtree: true,
-          childList: true,
-          attributes: true,
-          characterData: true
-        })
+        update(el, resolvedchange)
       }
       config = newConfig
     },
-    destroy () {
-      window.removeEventListener('resize', resolvedchange)
-      observer.disconnect()
-    }
+    destroy
   }
 }
---
+++ src/lib/repositioned.ts
@@ -0,0 +1,38 @@
+import equal from 'fast-deep-equal'
+import { bodyOffset } from './bodyoffset'
+
+export type ElementOffsets = Partial<ReturnType<typeof bodyOffset>>
+
+export function watchForPositionChange (el: HTMLElement, cb: (offset: Required<ElementOffsets>) => void) {
+  let lastoffset: ElementOffsets = {}
+  function watch () {
+    const offset = bodyOffset(el)
+    if (!equal(offset, lastoffset)) {
+      lastoffset = offset
+      cb(offset)
+    }
+  }
+  let observer = new MutationObserver(watch)
+  window.addEventListener('resize', watch, { passive: true })
+  observer = new MutationObserver(watch)
+  observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+    attributes: true,
+    characterData: true
+  })
+  watch()
+
+  return {
+    update (newEl: HTMLElement, newCb: (offset: Required<ElementOffsets>) => void) {
+      const changed = newEl !== el || newCb !== cb
+      el = newEl
+      cb = newCb
+      if (changed) watch()
+    },
+    destroy () {
+      window.removeEventListener('resize', watch)
+      observer.disconnect()
+    }
+  }
+}
