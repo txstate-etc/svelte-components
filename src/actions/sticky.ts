e45d91ed75ba5495c103adb39d03abe0ec1b97d8
Nick Wing <nwing@txstate.edu>
2021-04-25 09:02:09 -0500

    vast performance improvement for use:sticky on desktop, still lags on mobile

--- src/actions/sticky.ts
+++ src/actions/sticky.ts
@@ -1,5 +1,4 @@
-import { bodyOffset, SettableSubject } from '../lib'
-import { ElementOffsets, watchForPositionChange } from '../lib/repositioned'
+import { bodyOffset, SettableSubject, ElementOffsets, watchForPositionChange } from '../lib'
 
 export interface StickyArgs <T extends StickyStore = StickyStore> {
   target?: HTMLElement
@@ -11,19 +10,39 @@ export interface StickyStore {
   translateY?: number
 }
 
+let scrolled = false
+
 export function sticky (el: HTMLElement, config?: StickyArgs) {
   let floating: boolean
   let translateY: number
   let scrollstart = Number.MAX_SAFE_INTEGER
   let scrollend = Number.MAX_SAFE_INTEGER
 
-  function onscroll () {
-    const scroll = window.scrollY
-    const newTranslateY = Math.max(Math.min(scroll - 1, scrollend) - scrollstart, 0)
-    if (newTranslateY !== translateY) el.style.transform = `translateY(${newTranslateY}px)`
-    floating = scroll >= scrollstart
-    translateY = newTranslateY
-    config?.store?.update(v => ({ ...v, translateY, floating }))
+  function recalc (scroll: number) {
+    const newTranslateY = Math.max(Math.min(scroll, scrollend) - scrollstart, 0)
+    if (newTranslateY !== translateY) {
+      el.style.transform = `translateY(${newTranslateY}px)`
+      translateY = newTranslateY
+      floating = scroll >= scrollstart
+      config?.store?.update(v => ({ ...v, translateY, floating }))
+    }
+  }
+
+  function onscroll (e: Event) {
+    recalc(window.scrollY)
+  }
+
+  function onwheel (e: WheelEvent) {
+    e.preventDefault()
+    const scroll = window.scrollY + e.deltaY
+    recalc(scroll)
+    if (!scrolled) {
+      window.scrollTo({ top: scroll })
+      scrolled = true
+      setTimeout(() => {
+        scrolled = false
+      }, 0)
+    }
   }
 
   let lastOffsetParent: HTMLElement = el.offsetParent as HTMLElement
@@ -38,11 +57,12 @@ export function sticky (el: HTMLElement, config?: StickyArgs) {
     scrollstart = offset[0].top + el.offsetTop
     const containeroffset = config?.target ? offset[1] : offset[0]
     scrollend = Math.max(scrollstart, containeroffset.top + (config?.target ?? offsetParent).clientHeight - el.offsetHeight)
-    onscroll()
+    recalc(window.scrollY)
   }
 
   const { destroy, update } = watchForPositionChange([el.offsetParent as HTMLElement, config?.target], onpositionchange)
-  window.addEventListener('scroll', onscroll)
+  window.addEventListener('scroll', onscroll, { passive: true })
+  window.addEventListener('wheel', onwheel, { passive: false })
 
   function stickyUpdate (newConfig?: StickyArgs) {
     if (newConfig?.store !== config?.store) newConfig?.store?.update(v => ({ ...v, translateY, floating }))
@@ -52,6 +72,7 @@ export function sticky (el: HTMLElement, config?: StickyArgs) {
 
   function stickyDestroy () {
     window.removeEventListener('scroll', onscroll)
+    window.removeEventListener('wheel', onwheel)
     destroy()
   }
 
