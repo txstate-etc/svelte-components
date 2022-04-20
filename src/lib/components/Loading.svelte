d5b7abba73ef7c861473a085ccc9a8912a319d96
Nick Wing <nwing@txstate.edu>
2022-04-20 16:22:40 -0500

    more work on the Loading indicator component to make it work in a scrollable container, release 1.2.9

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
+  import { afterUpdate } from 'svelte'
   import ScreenReaderOnly from './ScreenReaderOnly.svelte'
   import { resize, ResizeStore } from '$lib/actions'
   export let loading: boolean|undefined
   export let height: string|undefined = undefined
   const store = new ResizeStore()
+
+  let containerelement: HTMLElement
+  let parentScrolls: boolean
+  afterUpdate(() => {
+    if (height || !containerelement) return
+    const parent = containerelement.offsetParent!
+    const overflowY = window.getComputedStyle(parent).overflowY
+    parentScrolls = overflowY === 'scroll' || overflowY === 'auto'
+  })
 </script>
 
 {#if loading}
-  <div class="container" style:height={height} style:position={height ? 'relative' : 'absolute'}>
-    <div use:resize={{ store }} class="loader" style:border-width="{$store.offsetWidth * 0.1}px"><ScreenReaderOnly>Loading</ScreenReaderOnly></div>
+  <div bind:this={containerelement} class="container" class:parentScrolls style:height={height} style:position={height ? 'relative' : undefined}>
+    <div use:resize={{ store }} class="loader" style:border-width="{$store.offsetWidth ? $store.offsetWidth * 0.1 : 0}px"><ScreenReaderOnly>Loading</ScreenReaderOnly></div>
   </div>
 {:else}
   <slot />
@@ -16,15 +26,20 @@
 
 <style>
   .container {
-    position: fixed;
+    position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+    z-index: 2;
     background-color: var(--loading-bg, var(--modal-bg, rgba(0, 0, 0, 0.7)));
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  .container.parentScrolls {
+    position: sticky;
+    margin-bottom: -100%;
   }
   .loader {
     box-sizing: content-box;
