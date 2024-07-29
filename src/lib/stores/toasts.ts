e8a32805a28f36b693c1ace46debaa3e1c84643c
Nick Wing <nwing@txstate.edu>
2024-07-29 15:28:37 -0500

    adjust timing for toasts and make it possible to preserve them while the mouse hovers, release 1.6.6

--- src/lib/stores/toasts.ts
+++ src/lib/stores/toasts.ts
@@ -7,7 +7,7 @@ export interface Toast {
   type?: 'error' | 'warning' | 'info' | 'success'
   /**
    * how long the message should appear on screen in milliseconds
-   * default 15 seconds
+   * default 8 seconds for errors and warnings, 5 seconds for success and info
    * use Number.MAX_SAFE_INTEGER for permanent
    */
   ttlMs?: number
@@ -17,28 +17,44 @@ export interface ToastStorage extends Required<Toast> {
   id: string
   /** when the toast was created, so we know when it expires */
   stamp: Date
+  /** True if auto-expiration has been suspended (e.g. user is hovering mouse over the toast) */
+  suspended: boolean
 }
 
 export class ToastStore extends Store<ToastStorage[]> {
+  timers: Record<string, number> = {}
+
   add (msg: string, type?: Toast['type'], ttlMs?: number) {
+    const resolvedType = type ?? 'error'
+    const id = randomid()
     const stored = {
-      id: randomid(),
+      id,
       stamp: new Date(),
       message: msg,
-      type: type ?? 'error',
-      ttlMs: ttlMs ?? 15000
+      type: resolvedType,
+      suspended: false,
+      ttlMs: ttlMs ?? (['error', 'warning'].includes(resolvedType) ? 8000 : 5000)
     }
     this.update(v => [...v.filter(t => t.message !== stored.message), stored])
-    setTimeout(() => { this.clean() }, stored.ttlMs)
+    this.timers[id] = setTimeout(() => { this.clean() }, stored.ttlMs)
   }
 
   close (id: string) {
     this.update(v => v.filter(t => t.id !== id))
   }
 
+  suspend (id: string) {
+    this.update(v => v.map(t => t.id !== id ? t : { ...t, suspended: true }))
+  }
+
+  resume (id: string) {
+    this.update(v => v.map(t => t.id !== id ? t : { ...t, suspended: false }))
+    this.clean()
+  }
+
   clean () {
     const now = new Date().getTime()
-    this.update(v => v.filter(t => now - t.stamp.getTime() < t.ttlMs))
+    this.update(v => v.filter(t => t.suspended || (now - t.stamp.getTime() < t.ttlMs)))
   }
 }
 
